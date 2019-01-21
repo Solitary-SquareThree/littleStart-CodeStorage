848eb4f277f96051278f2111cee3ce7acd52162e
zhouxin <zhouxin1@nuctech.com>
2019-01-21 17:12:17 +0800

    clear

--- fo-Dicom/DICOM/Network/IDicomCFindProvider.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace Dicom.Network {
-	public interface IDicomCFindProvider {
-		IEnumerable<DicomCFindResponse> OnCFindRequest(DicomCFindRequest request);
-	}
-}
--- fo-Dicom/DICOM/Network/IDicomCMoveProvider.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace Dicom.Network {
-	public interface IDicomCMoveProvider {
-		IEnumerable<DicomCMoveResponse> OnCMoveRequest(DicomCMoveRequest request);
-	}
-}
